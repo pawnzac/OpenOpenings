f380c27b75aa7f896c1a395942b9d01b693bc20f
pawnzac <pawnzac90@gmail.com>
2021-11-21 22:50:09 -0600

    Reorganized image for pyinstaller

--- gui.py
+++ gui.py
@@ -1,10 +1,27 @@
 import tkinter as tk, time, threading, random, queue
 from tkinter import ttk
 import chess
-from resource import piece_pics, blank_img
 import math
 from utility import *
 black_square_color = "#add8e6"
+
+piece_pics = {
+    "k" : tk.PhotoImage(file="img/black_king.png"),
+    "q" : tk.PhotoImage(file="img/black_queen.png"),
+    "r" : tk.PhotoImage(file="img/black_rook.png"),
+    "b" : tk.PhotoImage(file="img/black_bishop.png"),
+    "n" : tk.PhotoImage(file="img/black_knight.png"),
+    "p" : tk.PhotoImage(file="img/black_pawn.png"),
+    "K" : tk.PhotoImage(file="img/white_king.png"),
+    "Q" : tk.PhotoImage(file="img/white_queen.png"),
+    "R" : tk.PhotoImage(file="img/white_rook.png"),
+    "B" : tk.PhotoImage(file="img/white_bishop.png"),
+    "N" : tk.PhotoImage(file="img/white_knight.png"),
+    "P" : tk.PhotoImage(file="img/white_pawn.png")
+
+}
+
+blank_img = tk.PhotoImage(file="img/black_square.png")
 
 class UserInterface(object):
     def __init__(self, master, queue, end_command):
