c605ef153e6a78fb710aed383a3aa72d74bd65c2
pawnzac <pawnzac90@gmail.com>
2021-11-21 23:14:13 -0600

    Added license stuff

--- openopenings.py
+++ openopenings.py
@@ -1,3 +1,22 @@
+# OpenOpenings
+# Copyright 2021 pawnzac <pawnzac90@gmail.com>
+
+# This file is part of OpenOpenings.
+
+# OpenOpenings is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# OpenOpenings is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with OpenOpenings.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import tkinter as tk, time, threading, random, queue
 from tkinter import ttk
 from tkinter import filedialog
